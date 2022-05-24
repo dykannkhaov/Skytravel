f826f7d55060eeb461dcf441f624193f4a8f66aa
Dykann Khaov <dykann.khaov@gmail.com>
2022-05-24 16:11:08 +0200

    create Destinations' values type and the list od Destinations

---
+++ src/items/Destinations.tsx
@@ -0,0 +1,54 @@
+import type { Destination } from '../types/destination'
+
+const destinations: Destination[] = [
+  {
+    img: 'Destination1.jpeg',
+    name: 'London',
+    desc: 'The largest megalopolis in Europe.',
+    cost: '$9.000',
+    distance: 5500,
+    duration: 'for 7 nights',
+  },
+  {
+    img: 'Destination2.jpeg',
+    name: 'Marrakesh',
+    desc: 'The most visited city in Africa.',
+    cost: '$5.000',
+    distance: 6000,
+    duration: 'for 7 nights',
+  },
+  {
+    img: 'Destination3.jpeg',
+    name: 'Paris',
+    desc: 'The most beautiful city in the world.',
+    cost: '$10.000',
+    distance: 5800,
+    duration: 'for 7 nights',
+  },
+  {
+    img: 'Destination4.jpeg',
+    name: 'Tokyo',
+    desc: 'The largest megalopolis in the world.',
+    cost: '$8.000',
+    distance: 10800,
+    duration: 'for 7 nights',
+  },
+  {
+    img: 'Destination5.jpeg',
+    name: 'Giza',
+    desc: 'The most famous pyramid site in the world.',
+    cost: '$6.000',
+    distance: 9000,
+    duration: 'for 7 nights',
+  },
+  {
+    img: 'Destination6.jpeg',
+    name: 'Bangkok',
+    desc: 'The most visited city in the world.',
+    cost: '$4.000',
+    distance: 14000,
+    duration: 'for 7 nights',
+  },
+]
+
+export { destinations }
---
+++ src/types/destination.ts
@@ -0,0 +1,10 @@
+type Destination = {
+  img: string
+  name: string
+  desc: string
+  cost: string
+  distance: number
+  duration: string
+}
+
+export type { Destination }

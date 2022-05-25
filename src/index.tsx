19860d0e01d08ef238cd301306e991b1c8ea16f5
Dykann Khaov <dykann.khaov@gmail.com>
2022-05-25 11:21:59 +0200

    wrap AppProvider in the tree for react-query usage

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import AppProvider from './context/app-providers'
 import { ThemeProvider } from './context/theme-context'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <AppProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AppProvider>
   </React.StrictMode>,
 )
